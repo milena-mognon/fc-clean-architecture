88b8f6a788bb9d12a4589196b6adf246bf7e8720
milena-mognon <milenamognon@gmail.com>
2022-05-25 12:51:26 -0300

    domain service - add reward points to customer

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -28,6 +28,7 @@ export class Customer {
   private _name: string;
   private _address!: Address;
   private _active: boolean = true;
+  private _rewardPoints: number = 0;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -35,8 +36,16 @@ export class Customer {
     this.validate(); // garante a validação
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints;
   }
 
   isActive(): boolean {
@@ -70,6 +79,10 @@ export class Customer {
 
   deactivate() {
     this._active = false;
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardPoints += points;
   }
 
   set address(address: Address) {
--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,3 +1,4 @@
+import { Customer } from '../entity/customer';
 import { Order } from '../entity/Order';
 import { OrderItem } from '../entity/OrderItem';
 import { OrderService } from './order.service';
@@ -13,5 +14,16 @@ describe('Order Service unit test', () => {
     const total = OrderService.total([order1, order2]);
 
     expect(total).toBe(500);
+  });
+
+  it('should place an order', () => {
+    const customer = new Customer('c1', 'Customer 1');
+
+    const item1 = new OrderItem('i1', 'Item 1', 10, 'p1', 1);
+
+    const order = OrderService.placeOrder(customer, [item1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
   });
 });
