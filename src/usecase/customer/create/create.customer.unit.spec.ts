4d5d3d9407de61aee8380f3825448de1f43b52f8
milena-mognon <milenamognon@gmail.com>
2022-11-15 16:27:38 -0300

    feat: create customer use case - validation

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -40,4 +40,28 @@ describe('Unit Test Create Customer', () => {
       },
     });
   });
+
+  it('should throw an error when name is missing', async () => {
+    const customerRepository = MockRepository();
+
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.name = '';
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      'Name is required',
+    );
+  });
+
+  it('should throw an error when street is missing', async () => {
+    const customerRepository = MockRepository();
+
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+    input.address.street = '';
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      'Street is required',
+    );
+  });
 });
