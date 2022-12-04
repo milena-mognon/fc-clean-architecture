bd95c5cf8f4bb0586783580a2c9e1e48cea06b07
milena-mognon <milenamognon@gmail.com>
2022-12-04 14:59:12 -0300

    test: initial e2e test config

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,33 @@
+import { app, sequilize } from '../express';
+import request from 'supertest';
+
+describe('End 2 End test for customer', () => {
+  beforeEach(async () => {
+    await sequilize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequilize.close();
+  });
+
+  it('should create a customer', async () => {
+    const response = await request(app)
+      .post('/customer')
+      .send({
+        name: 'Jane Doe',
+        address: {
+          street: 'Street 1',
+          city: 'City 1',
+          number: 123,
+          zip: '12345',
+        },
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('Jane Doe');
+    expect(response.body.name.address.street).toBe('Street 1');
+    expect(response.body.address.city).toBe('City 1');
+    expect(response.body.address.number).toBe(123);
+    expect(response.body.address.zip).toBe('12345');
+  });
+});
